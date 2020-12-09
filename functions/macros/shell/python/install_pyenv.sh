348ac18a539db3c89d6f6daae911ce9ae84a70e0
Zach McFadden <zach.mcfadden@workday.com>
2020-12-09 13:37:25 -0800

    local stuff

--- functions/macros/shell/python/install_pyenv.sh
+++ functions/macros/shell/python/install_pyenv.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+bash_rc() {
+ if [[ $SHELL == '/bin/zsh' ]]; then
+  export BASH_RC='~/.zshrc'
+ else
+  export BASH_RC='~/.bashrc'
+ fi
+}
+
 curl https://pyenv.run | bash
+
+echo "export PATH=\"$HOME/.pyenv/bin:$PATH\"" | $BASH_RC
---
+++ functions/macros/shell/xopen.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# xdg sucks and i dont like typing it 
+
+xopen () {
+  xdg-open $1
+}
